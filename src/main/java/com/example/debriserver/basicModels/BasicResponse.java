5ab6bbd6244c111132ad7d0d7a2292ca2e9eda5f
LeeJiHo <destiny3910@gmail.com>
2022-07-04 00:46:08 +0900

    API Template

--- src/main/java/com/example/debriserver/basicModels/BasicResponse.java
+++ src/main/java/com/example/debriserver/basicModels/BasicResponse.java
@@ -1,19 +1,46 @@
 package com.example.debriserver.basicModels;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import static com.example.debriserver.basicModels.BasicServerStatus.SUCCESS;
 
 /**
  * Basic Response Model class
  *
  * @author Rookie/이지호
  * @since 22.06.26
- * @modify 수정자 이름 적어주세요
- * @updated 수정일자 적어주세요
+ * @modify Rookie/이지호
+ * @updated 22.07.01
  * */
 
 @Getter
-@Setter
-public class BasicResponse {
-    //TODO JSON으로 서버 교육세션과 같은 Response Format을 만들어주세요
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "returnCode", "returnMsg", "result"})
+public class BasicResponse<L> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final int returnCode;
+    private final String returnMsg;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private L result;
+
+    //Request Success
+    public BasicResponse(L result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.returnCode = SUCCESS.getReturnCode();
+        this.returnMsg = SUCCESS.getReturnMsg();
+        this.result = result;
+    }
+
+    //Request Fail
+    public BasicResponse(BasicServerStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.returnCode = status.getReturnCode();
+        this.returnMsg = status.getReturnMsg();
+    }
 }
