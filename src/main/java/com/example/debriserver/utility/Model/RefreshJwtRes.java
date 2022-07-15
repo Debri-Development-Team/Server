586b3052cd7d9cf6e4fffa4bcc3c37c3135455e4
LeeJiHo <destiny3910@gmail.com>
2022-07-15 23:15:11 +0900

    feature/Refresh jwt, fix/add git ignore parameters

--- src/main/java/com/example/debriserver/utility/Model/RefreshJwtRes.java
+++ src/main/java/com/example/debriserver/utility/Model/RefreshJwtRes.java
@@ -1,2 +1,14 @@
-package com.example.debriserver.utility.Model;public class RefreshJwtRes {
+package com.example.debriserver.utility.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RefreshJwtRes {
+    private int userIdx;
+    private String accessToken;
+    private String refreshToken;
 }
