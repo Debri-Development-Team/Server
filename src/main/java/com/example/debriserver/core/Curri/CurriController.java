a283cbc797dcae1c72e8cb6e3ba7b4bbbdc4e478
= <=>
2022-07-26 17:43:38 +0900

    커리 API ã중간 단계

--- src/main/java/com/example/debriserver/core/Curri/CurriController.java
+++ src/main/java/com/example/debriserver/core/Curri/CurriController.java
@@ -6,6 +6,7 @@ import com.example.debriserver.utility.jwtUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +35,18 @@ public class CurriController {
 
     /*
     *  커리큘럼 제작 API
-    *   /api/curri/creat
-    * */
+    *   - 1. Lecture 테이블에서 강의자료를 선택
+    *   - 2. 선택한 자료를 Curri_Lecture(이하 CL)에 저장
+    *   - 3. CL에 저장한 자료를 한가지 CurriIdx에 매칭
+    *   - 받아야 할 값 : 선택된 Lecture에 대한 정보
+    *   - 반환할 값 : 성공 여부 및 생성된 CurriIdx
+    *   - 저장 할 값 : Curriculum 테이블 꽉꽉
+    *   [POST]: localhost:8521/api/curri/create
+    */
+
+    @PostMapping("/create")
+    public
+
 
     /*
     *   커리큘럼 수정 API
